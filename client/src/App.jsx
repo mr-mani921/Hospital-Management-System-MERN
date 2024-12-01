bb1ebf9c5b09adde4aa38a102ca406125912c4ee
M. Usman Raza <mu951882@gmail.com>
2024-12-01 21:18:04 +0500

    initial first setup of front end and / route for Home component

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+const App = () => {
+  return (
+    <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+    </div>
+  );
+};
+
+export default App;
